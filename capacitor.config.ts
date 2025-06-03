979a61479724bf3cb9ef8a32dc37ebd96b62832b
Walter Banda <walterbanda98@gmail.com>
2025-06-03 18:44:05 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'accounts-agents',
+  webDir: 'dist'
+};
+
+export default config;
